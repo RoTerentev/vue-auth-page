0493c37901f923028fb7734e3f44642327a711d6
Roman Terentev <roman.dv.93@gmail.com>
2019-04-17 14:45:56 +1000

    ADD: element ui lib

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 import Vue from 'vue';
+
 import App from './App.vue';
 import router from './router';
 import store from './store/index';
+
+// Global styles
+import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.config.productionTip = false;
 
