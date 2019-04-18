8e42e9984d64da877b03b23a487cc91edc0da283
Roman Terentev <roman.dv.93@gmail.com>
2019-04-18 15:50:20 +1000

    ADD: complete auth store module

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,18 @@
 import Vue from 'vue';
-import Vuex from 'vuex';
+import Vuex, { StoreOptions } from 'vuex';
 
-import auth from './modules/auth';
+import { auth } from './modules/auth';
+
+export interface RootState {}
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
+const store: StoreOptions<RootState> = {
   strict: process.env.NODE_ENV !== 'production',
+  state: {},
   modules: {
     auth
   }
-});
+};
+
+export default new Vuex.Store<RootState>(store);
--- src/store/modules/auth.ts
+++ src/store/modules/auth.ts
@@ -1,27 +1,148 @@
-class AuthState {
-  // TODO: replace by real User type/class
-  public person: any;
+import { AuthCredentials } from '@/ts-declare/types/AuthCredentials';
+import { RegisterCredentials } from '@/ts-declare/types/RegisterCredentials';
 
-  constructor() {
-    this.person = null;
-  }
+import { Module, ActionTree, MutationTree, GetterTree } from 'vuex';
+
+import { Person } from '@/ts-declare/classes/Person';
+import prettyPhone from '@/utils/pretty-phone';
+import { RootState } from '../index';
+
+export enum AUTH_MUTATIONS {
+  PERSON = 'PERSON',
+  ERROR = 'ERROR',
+  TOKEN = 'TOKEN',
+  PROCESSING = 'PROCESSING'
 }
 
-const getters = {
-  isLogin(state: AuthState): boolean {
-    return state.person !== null;
+export interface AuthState {
+  person: Person | null;
+  token: string;
+  error: string;
+  isProcessing: boolean;
+}
+
+export const getters: GetterTree<AuthState, RootState> = {
+  isLogin(authState): boolean {
+    return authState.token !== '';
+  },
+  prettyPhone(authState): string {
+    if (authState.person !== null) {
+      return prettyPhone(authState.person.phone);
+    }
+    return '';
   }
 };
 
+export const actions: ActionTree<AuthState, RootState> = {
+  signIn({ commit }, credentials: AuthCredentials): void {
 
-const actions = {};
+    commit(AUTH_MUTATIONS.PROCESSING, true);
 
-const mutations = {};
+    new Promise((resolve, reject) => {
 
-export default {
-  namespaced: true,
-  state: new AuthState(),
+      // SOME AUTH IMPLEMENTATION SHOULD BE HERE
+
+      // stub
+      if (Math.random() > 0.5) {
+        resolve({
+          status: 'ok', data: {
+            person: {
+              id: 100,
+              firstName: 'Vasya',
+              middleName: '',
+              lastName: 'Pup',
+              phone: credentials.phone
+            },
+            token: 'onesuperdupertokenfromcryptoworld'
+          }
+        });
+      }
+
+      resolve({
+        status: 'err', data: {
+          msg: 'Bad credentials'
+        }
+      });
+
+    })
+      .then((resp: any) => {
+        commit(AUTH_MUTATIONS.PROCESSING, false);
+        if (resp.status === 'err') {
+          commit(AUTH_MUTATIONS.ERROR, resp.data.msg);
+        } else {
+          commit(AUTH_MUTATIONS.TOKEN, resp.data.token);
+          commit(AUTH_MUTATIONS.PERSON, resp.data.person);
+        }
+      })
+      .catch((err) => {
+        commit(AUTH_MUTATIONS.PROCESSING, false);
+        commit(AUTH_MUTATIONS.ERROR, err.toSting());
+      });
+  },
+  signUp({ commit }, credentials: RegisterCredentials): Promise<void> {
+    commit(AUTH_MUTATIONS.PROCESSING, true);
+
+    return new Promise((resolve, reject) => {
+
+      // THE IMPLEMENTATION OF REGISTRATION SHOULD BE HERE
+
+      // stub
+      if (Math.random() > 0.5) {
+
+        commit(AUTH_MUTATIONS.PROCESSING, false);
+        commit(AUTH_MUTATIONS.PERSON, {
+          id: 100,
+          firstName: credentials.firstName,
+          middleName: credentials.middleName,
+          lastName: credentials.lastName,
+          phone: credentials.phone
+        });
+        resolve();
+      } else {
+        commit(AUTH_MUTATIONS.PROCESSING, false);
+        commit(AUTH_MUTATIONS.ERROR, 'Bad credentials');
+        reject();
+      }
+    });
+  },
+  signOut({ commit }): void {
+    commit(AUTH_MUTATIONS.PERSON, null);
+    commit(AUTH_MUTATIONS.TOKEN, '');
+  }
+};
+
+export const mutations: MutationTree<AuthState> = {
+  [AUTH_MUTATIONS.PROCESSING](authState: AuthState, isProcessing: boolean) {
+    if (isProcessing) {
+      authState.error = '';
+    }
+    authState.isProcessing = isProcessing;
+  },
+  [AUTH_MUTATIONS.ERROR](authState: AuthState, error: string) {
+    authState.error = error;
+  },
+  [AUTH_MUTATIONS.TOKEN](authState: AuthState, token: string) {
+    authState.token = token;
+  },
+  [AUTH_MUTATIONS.PERSON](authState: AuthState, person: Person | null) {
+    authState.person = person as Person;
+  }
+};
+
+export const state: AuthState = {
+  person: null,
+  token: '',
+  error: '',
+  isProcessing: false
+};
+
+const namespaced: boolean = true;
+
+export const auth: Module<AuthState, RootState> = {
+  namespaced,
+  state,
   getters,
   actions,
   mutations
 };
+
