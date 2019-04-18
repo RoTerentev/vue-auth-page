ed11ebe6e150d333034a4e38a3e0452c6002f220
Roman Terentev <roman.dv.93@gmail.com>
2019-04-18 15:49:37 +1000

    WIP: complete SignIn

--- src/views/auth/SignIn.vue
+++ src/views/auth/SignIn.vue
@@ -6,7 +6,7 @@
     <Main>
       <Row type='flex' justify='center'>
         <Col :xs='24' :sm='16' :md='12' :lg='8' :xl='6'>
-          <Form status-icon :model='form' :rules='valid' ref='form:auth'>
+          <Form status-icon :model='form' :rules='validateRules' @keyup.enter.prevent.native="submitForm" ref='form:auth'>
             <FormItem class='mb' label='Телефон' prop='phone'>
               <FieldPhone v-model='form.phone'/>
             </FormItem>
@@ -14,7 +14,15 @@
               <FieldPassword v-model='form.password'/>
             </FormItem>
             <FormItem class='mt'>
-              <Button type='primary' @click='submitForm'>Войти</Button>
+              <Alert v-if="alert.msg !== ''" center  :type="alert.type" :closable="alert.close" style="margin-bottom: 20px;">
+                <template slot="title">
+                  <span class="fz16">{{ alert.title }}</span>
+                </template>
+                <template slot="default">
+                  <span class="fz16">{{ alert.msg }}</span>
+                </template>
+              </Alert>
+              <Button type='primary' @click.prevent.native='submitForm' :loading="isAuthProcessing" :disabled="isLogin">Войти</Button>
             </FormItem>
           </Form>
         </Col>
@@ -27,7 +35,8 @@
 </template>
 
 <script lang="ts">
-import { Vue, Component } from 'vue-property-decorator';
+import Vue from 'vue';
+import Component from 'vue-class-component';
 // TODO: add|check tree shaking
 import {
   Button,
@@ -38,8 +47,11 @@ import {
   Header,
   Container,
   Row,
-  Col
+  Col,
+  Alert
 } from 'element-ui';
+
+import { Action, namespace } from 'vuex-class';
 
 import FieldPhone, {
   validator as phoneValidator
@@ -48,13 +60,14 @@ import FieldPassword, {
   validator as passwordValidator
 } from '@/components/form/fields/password.vue';
 
-declare class AuthForm {
-  phone: string;
-  password: string;
-}
+import { Person } from '@/ts-declare/classes/Person';
+import { AuthCredentials } from '@/ts-declare/types/AuthCredentials';
+import fullname from '@/utils/fullname';
+
+const AuthStore = namespace('auth');
 
 @Component({
-  name: 'register-view',
+  name: 'sign-in-view',
   components: {
     Button,
     Form,
@@ -66,32 +79,68 @@ declare class AuthForm {
     Footer,
     Container,
     Row,
-    Col
+    Col,
+    Alert
   }
 })
 export default class SingInView extends Vue {
-  private form: AuthForm = {
+
+  public get alert(): {title: string, msg: string, type: string, close: boolean } {
+    if (this.authError && this.authError !== '') {
+      return {title: 'Ошибка', msg: this.authError, type: 'error', close: true};
+    }
+
+    if (this.isLogin) {
+      return {title: 'Вы вошли под номером', msg: this.prettyPhone, type: 'success', close: false};
+    }
+
+    if (this.$route.query.new && this.person) {
+      return {title: 'Вы зарегистрировались', msg: `под номером ${this.prettyPhone}, как ${fullname(this.person)}`, type: 'info', close: false};
+    }
+
+    return {title: '', msg: '', type: '',  close: true};
+  }
+
+  public form: AuthCredentials = {
     phone: '',
     password: ''
   };
 
-  private valid = {
+  public validateRules = {
     phone: [{ required: true, validator: phoneValidator, trigger: 'change' }],
     password: [
       { required: true, validator: passwordValidator, trigger: 'change' }
     ]
   };
+  @AuthStore.State((state) => state.person)
+  private readonly person!: Person | null;
+
+  @AuthStore.State((state) => state.isProcessing)
+  private readonly isAuthProcessing!: boolean;
+
+  @AuthStore.State((state) => state.error)
+  private readonly authError!: string;
+
+  @AuthStore.Getter('isLogin')
+  private readonly isLogin!: boolean;
+
+  @AuthStore.Getter('prettyPhone')
+  private readonly prettyPhone!: string;
+
+  @AuthStore.Action
+  private signIn!: (arg: AuthCredentials) => void;
 
   public submitForm(): void {
     const form = this.$refs['form:auth'] as Form;
     form.validate((valid: boolean) => {
       if (valid) {
-        alert('submit!');
-      } else {
-        console.log('error submit!!');
-        return false;
+        // TODO: refactoring for phone prefix logic
+        const formData: AuthCredentials = {password: this.form.password, phone: `+7${this.form.phone}`};
+        this.signIn(formData);
       }
+      return false;
     });
   }
+
 }
 </script>
